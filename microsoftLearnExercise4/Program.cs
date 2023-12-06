501ed97fbcec3865c7a13e836116333ca96d4a1f
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-12-06 12:34:02 +0100

    Add project files.

---
+++ microsoftLearnExercise4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace microsoftLearnExercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "abc123";
+            char[] valueArray = value.ToCharArray();
+            Array.Reverse(valueArray);
+            //var result = new string(valueArray);
+            var result = String.Join(",", valueArray);
+
+            Console.WriteLine(result);
+
+            string[] items = result.Split(",");
+            foreach ( string item in items )
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
